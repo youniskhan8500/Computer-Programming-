a327b6b9c5c9020a2e4fc70b4ddb702b61fe94c4
youniskhan8500 <youniskhan8500@gmail.com>
2026-03-05 01:20:59 +0500

    Update LAB 3 LABWORK 2.cpp

--- LAB 4/LAB WORK/LAB 3 LABWORK 2.cpp
+++ LAB 4/LAB WORK/LAB 3 LABWORK 2.cpp
@@ -1,7 +1,7 @@
-// Name: Abdul Ahad
-// Department: Electrical Engineering (AI)
-// Registration No: BF25NWELE0692
-// Section: B
+// Name: M.Younas Khan 
+// Department: Electrical Engineering (POWER)
+// Registration No: BF25NWELE0741
+// Section: A
 
 
 
@@ -63,3 +63,4 @@ int main()
 
 return 0;
 }
+
