27aaf59bd92f8414a09c8aa36155e997fcd06b64
youniskhan8500 <youniskhan8500@gmail.com>
2026-03-05 00:28:41 +0500

    Add files via upload

---
+++ LAB 1 /LAB WORK/LAB 1 TASK 1.cpp
@@ -0,0 +1,17 @@
+// Name: M.Younas Khan 
+// Department: Electrical Engineering (POWER)
+// Registration No: BF25NWELE0741
+// Section: A
+
+
+
+
+#include <iostream> //include library for input and output
+using namespace std; // allow standard names like "cout and cin" without std:
+// function main begins program execution
+int main()// it is sttarting point of program
+    {
+
+
+
+}
