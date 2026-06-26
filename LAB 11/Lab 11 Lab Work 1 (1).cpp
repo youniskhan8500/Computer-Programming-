e93f966045ee56aa664548a4646e7bfdf8bafd3f
youniskhan8500 <youniskhan8500@gmail.com>
2026-06-26 23:55:13 +0500

    Add files via upload

---
+++ LAB 11/Lab 11 Lab Work 1 (1).cpp
@@ -0,0 +1,18 @@
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Declaring and initializing a string variable
+    string message = "Hello Engineering World";
+
+    // Displaying the original message
+    cout << "Message: " << message << endl;
+
+    // Calculating and displaying the length of the string using built-in function
+    cout << "Length of string: " << message.length() << endl;
+
+    return 0;
+}
---
+++ LAB 11/Lab 11 Lab Work 2.cpp
@@ -0,0 +1,21 @@
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Declaring and initializing first name
+    string firstName = "Shahzad ";
+
+    // Declaring and initializing last name
+    string lastName = "Faisal";
+
+    // Concatenating first name and last name with a space in between
+    string fullName = firstName + " " + lastName;
+
+    // Displaying the full name
+    cout << "Full Name: " << fullName << endl;
+
+    return 0;
+}
